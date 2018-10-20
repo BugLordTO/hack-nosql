4d601ee2ee2ef3bebbc27ecf924f309810005d0c
Somkiet <somsor@perfenterprise.com>
2018-10-20 13:31:54 +0700

    Redesign API

--- WalletSampleApi/Controllers/HackController.cs
+++ WalletSampleApi/Controllers/HackController.cs
@@ -25,48 +25,55 @@ namespace WalletSampleApi.Controllers
             _coinPriceUpdateCollection = _database.GetCollection<CoinPriceUpdate>("CoinPriceUpdates");
         }
 
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            var customers = _customerCollection.Find(it => true).ToList();
+            return customers.Select(it => it.Username).ToList();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<CustomerWallet> Get(string id)
+        {
+            return _customerCollection.Find(it => it.Username.Equals(id, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] CoinPriceUpdate updateCoin)
+        {
+            _coinPriceUpdateCollection.InsertOne(updateCoin);
+        }
+        
         [HttpGet]
         public CoinPriceUpdate GetCoinPrice()
         {
-            return _coinPriceUpdateCollection.Find(it=>true).SortByDescending(it=>it.At).FirstOrDefault();
+            return _coinPriceUpdateCollection.Find(it => true).SortByDescending(it => it.At).FirstOrDefault();
         }
 
         [HttpGet("{id}")]
         public CoinPrice GetCoinPrice(string id)
         {
             var lastUpdateCoin = GetCoinPrice();
-            return lastUpdateCoin.PriceList.FirstOrDefault(it=>it.Symbol.Equals(id,StringComparison.CurrentCultureIgnoreCase));
+            return lastUpdateCoin.PriceList.FirstOrDefault(it => it.Symbol.Equals(id, StringComparison.CurrentCultureIgnoreCase));
         }
 
         [HttpPost]
         public void BuyCoin(string username, string symbol, double coinamount){
             var coin = GetCoinPrice(symbol);
 
-            var customer = _customerCollection.Find(it=>it.Username.Equals(username,StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+            var customer = _customerCollection.Find(it => it.Username.Equals(username, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
             customer.Coins.Add(new CustomerCoin{
                 Symbol = coin.Symbol,
                 BuyingAt = DateTime.UtcNow,
                 BuyingRate = coin.Buy,
                 USDValue  = coinamount * coin.Buy,
             });
-            _customerCollection.ReplaceOne(it=>it.Username.Equals(username,StringComparison.CurrentCultureIgnoreCase), customer);
-        }
-
-        [HttpPost]
-        public void UpdateCoin([FromBody]CoinPriceUpdate request){
-            _coinPriceUpdateCollection.InsertOne(request);
-        }
-
-        [HttpGet]
-        public IEnumerable<CustomerWallet> GetCustomers(){
-            return _customerCollection.Find(it=>true).ToList();
+            _customerCollection.ReplaceOne(it => it.Username.Equals(username, StringComparison.CurrentCultureIgnoreCase), customer);
         }
         
-        [HttpGet("{id}")]
-        public CustomerWallet GetCustomer(string id){
-            return _customerCollection.Find(it=>it.Username.Equals(id,StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
-        }
-
         [HttpPost("{id}")]
         public void RegisterCustomer(string id){
             _customerCollection.InsertOne(new CustomerWallet{
@@ -75,44 +82,5 @@ namespace WalletSampleApi.Controllers
             });
         }
 
-        // GET api/values
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
-        {
-            return new string[] { "jdoe", "ptparker" };
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<CustomerWallet> Get(string id)
-        {
-            return new CustomerWallet
-            {
-                Username = "jdoe",
-                Coins = new List<CustomerCoin>
-                {
-                    new CustomerCoin
-                    {
-                        Symbol = "BTC",
-                        BuyingRate = 6565.25,
-                        BuyingAt = new DateTime(2018, 10, 9, 9, 32, 23),
-                        USDValue = 6500
-                    },
-                    new CustomerCoin
-                    {
-                        Symbol = "ETH",
-                        BuyingRate = 203.47,
-                        BuyingAt = new DateTime(2018, 9, 7, 12, 38, 33),
-                        USDValue = 200.23
-                    },
-                },
-            };
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] CoinPriceUpdate updateCoin)
-        {
-        }
     }
 }
