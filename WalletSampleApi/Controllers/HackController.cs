1084a4b7bb4bf39a585f05b3452be496072e23f8
Somkiet <somsor@perfenterprise.com>
2018-10-20 12:14:27 +0700

    connection mongo implement api

--- WalletSampleApi/Controllers/HackController.cs
+++ WalletSampleApi/Controllers/HackController.cs
@@ -4,18 +4,39 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WalletSampleApi.Models;
+using MongoDB.Driver;
 
 namespace WalletSampleApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class HackController : ControllerBase
     {
+        private IMongoDatabase _database;
+        private IMongoCollection<CustomerWallet> _customerCollection;
+        private IMongoCollection<CoinPriceUpdate> _coinPriceUpdateCollection;
+
+        public HackController()
+        {
+            var settings = MongoClientSettings.FromUrl(new MongoUrl("mongodb://admin:admin1234@ds237373.mlab.com:37373/hackcoin"));
+            var mongoClient = new MongoClient(settings);
+            _database = mongoClient.GetDatabase("hackcoin");
+            _customerCollection = _database.GetCollection<CustomerWallet>("CustomerWallets");
+            _coinPriceUpdateCollection = _database.GetCollection<CoinPriceUpdate>("CoinPriceUpdates");
+        }
+
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public CoinPriceUpdate GetCoinPrice()
         {
-            return new string[] { "jdoe", "ptparker" };
+            return _coinPriceUpdateCollection.Find(it=>true).SortByDescending(it=>it.At).FirstOrDefault();
+        }
+
+        [HttpGet("{id}")]
+        public CoinPrice GetCoinPrice(string id)
+        {
+            var lastUpdateCoin = GetCoinPrice();
+            return lastUpdateCoin.PriceList.FirstOrDefault(it=>it.Symbol.Equals(id,StringComparison.CurrentCultureIgnoreCase));
         }
 
         // GET api/values/5
