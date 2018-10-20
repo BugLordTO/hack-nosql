9ee030ecf1f187bdac9fa6628d3d2d1cff4350fb
powerofwind <powerofwind10@gmail.com>
2018-10-20 15:06:50 +0700

    edit lick

--- coin/src/app/selling/selling.page.ts
+++ coin/src/app/selling/selling.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-selling',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SellingPage implements OnInit {
 
-  constructor() { }
+  constructor(public navCtrl: NavController) { }
 
   ngOnInit() {
+  }
+  
+  home(){
+    this.navCtrl.navigateForward("home");
   }
 
 }
