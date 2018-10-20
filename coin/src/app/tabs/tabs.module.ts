6cb7cac5aa63a6fc45b50a22372bfd558516d87f
MekaMask <agapevb35@gmail.com>
2018-10-20 15:47:59 +0700

    config app-ruoting.module

--- coin/src/app/tabs/tabs.module.ts
+++ coin/src/app/tabs/tabs.module.ts
@@ -1,5 +1,4 @@
 import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -7,9 +6,9 @@ import { FormsModule } from '@angular/forms';
 import { TabsPageRoutingModule } from './tabs.router.module';
 
 import { TabsPage } from './tabs.page';
-import { ContactPageModule } from '../contact/contact.module';
 import { AboutPageModule } from '../about/about.module';
 import { HomePageModule } from '../home/home.module';
+import { UpdatePageModule } from '../update/update.module';
 
 @NgModule({
   imports: [
@@ -19,7 +18,7 @@ import { HomePageModule } from '../home/home.module';
     TabsPageRoutingModule,
     HomePageModule,
     AboutPageModule,
-    ContactPageModule
+    UpdatePageModule
   ],
   declarations: [TabsPage]
 })
